8dd8093d3e3d5bbe960ddd2abc79e8ac370dc48d
Eric Chen <eric.chen891@gmail.com>
2019-06-06 03:52:04 -0700

    Write Character Javadocs

--- Character.java
+++ Character.java
@@ -18,6 +18,15 @@ public class Character extends Movable
 
     private Room currentRoom;
 
+    /**
+    * Character() - Constructor for Character class
+    * @param tree BSPTree for the character
+    * @param x initial x pos coordinate
+    * @param y initial y pos coordinate
+    * @param lv initial level
+    * @param hp initial health
+    * @param dmg initial damage done
+    */
     public Character(BSPTree tree, double x, double y, int lv, int hp, int dmg)
     {
         super(x, y);
@@ -31,31 +40,53 @@ public class Character extends Movable
         damage = dmg;
     }
 
+    /**
+    * getCurrHp() - Returns current HP value
+    * @return current HP
+    */
     public int getCurrHp()
     {
         return currHp;
     }
 
+    /**
+    * getMaxHp() - Returns highest HP
+    * @return highest HP
+    */
     public int getMaxHp()
     {
         return maxHp;
     }
 
+    /**
+    * getBulletsFired() - Retuns ArrayList of bullets fired
+    * @return ArrayList of bullets fired
+    */
     public ArrayList<Bullet> getBulletsFired()
     {
         return bulletsFired;
     }
 
+    /**
+    * gameOver() - Ends the game if current HP < 0
+    */
     public boolean gameOver()
     {
         return (currHp <= 0);
     }
 
+    /**
+    * levelUp() - Incerements the level
+    */
     public void levelUp()
     {
         level++;
     }
 
+    /**
+    * addHealth() - Adds specified amount health to the character
+    * @param amount the amount of health to add to the character
+    */
     public void addHealth(int amount)
     {
         currHp += amount;
@@ -64,11 +95,19 @@ public class Character extends Movable
             currHp = maxHp;
     }
 
+    /**
+    * addDamage() - Adds specified amount damage to the character
+    * @param amount the amount of damage to add to the character
+    */
     public void addDamage(int amount)
     {
         damage += amount;
     }
 
+    /**
+    * scanRooms() - Scans rooms in specified leaves on ArrayList
+    * @param leaves ArrayList of leaves to scan
+    */
     public void scanRooms(ArrayList<BSPLeaf> leaves)
     {
         for (BSPLeaf leaf : leaves)
@@ -83,6 +122,10 @@ public class Character extends Movable
         }
     }
 
+    /**
+    * attack() - Attacks on mouse click
+    * @param event mouse click
+    */
     public void attack(MouseEvent event)
     {
         Bullet temp = new Bullet(bspTree, x, y, 15, 10);
@@ -90,6 +133,12 @@ public class Character extends Movable
         temp.launch(event);
     }
 
+    /**
+    * move() - Moves in the specified direction and distance
+    *
+    * @param direction direction toward which to move
+    * @param delta distance to move
+    */
     @Override
     public void move(char direction, double delta)
     {
